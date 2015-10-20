c5f4874c50a1829476e82b2a550ac4ab836ea9c8
deddokatana <deddokatana@gmail.com>
2015-10-20 22:26:42 +0100

    WORKING TOGGLE BUTTON ... ADD instructions!

--- Main.py
+++ Main.py
@@ -9,23 +9,25 @@ builder.add_from_file("MainWindow.glade")
 MainWindow = builder.get_object("MainWindow")
 ToggleServerBtn = builder.get_object("ToggleServerOn")
 
-#define functions here
+
+# check signals->[handler] column  in glade object properties bar
+# to determine the function names.
+# all handlers take 2 variables.
 class Handler():
+# define functions here
     def on_MainWindow_destroy(self, *args):
         Gtk.main_quit(*args)
         quit
-    def on_ToggleServerOn_toggled(self, *args):
-        print("hello World!")
         return 1
-    def ToggleServerOn_toggled_cb(self, *args):
-        pass
+    def on_ToggleServerOn_toggled(self, *args): 
+        if ToggleServerBtn.get_active(): #get active is boolean! toggle button
+            print("hello World - Pressed Down!")
+        else:
+            print("Pressed Pressed Up!")
+        return 1
 
-#link signals to functions here - No LOOPS ALLOWED without a sub process
-###handlers = {
-###    "onDeleteWindow": Gtk.main_quit,
-###    "on_ToggleServerOn_toggled": ToggleServerOnPress,
-###    "ToggleServerOn_toggled_cb": ToggleServerOnDepress,
-###}
+
+#Handler is the class above.
 builder.connect_signals(Handler())
 
 #Display window once loaded.
